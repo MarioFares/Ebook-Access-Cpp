9a8f35f2700c0d2cb01d419429e4926247586c6f
Mario Fares <mariofares02@gmail.com>
2021-09-26 00:43:13 -0400

    Cleaned headers + filesystem > dirIterator & QFileInfo

--- addbooksdialog.cpp
+++ addbooksdialog.cpp
@@ -5,25 +5,16 @@
 #include "queries.h"
 #include "bulkdetailsdialog.h"
 
-#include <vector>
-#include <filesystem>
-
-#include <QMessageBox>
+#include <QFileInfo>
 #include <QFileDialog>
 #include <QProgressBar>
-#include <QThread>
-
-#include <iostream>
-QProgressBar *progressBar;
-
-namespace fs = std::filesystem;
+#include <QDirIterator>
 
 addBooksDialog::addBooksDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::addBooksDialog)
 {
     ui->setupUi(this);
-    progressBar = ui->progressBar;
 }
 
 addBooksDialog::~addBooksDialog()
@@ -37,7 +28,6 @@ void addBooksDialog::on_buttonClose_clicked()
     close();
 }
 
-
 void addBooksDialog::on_buttonBrowseFolders_clicked()
 {
     QString dir = QFileDialog::getExistingDirectory(this, tr("Choose Folder"),
@@ -48,66 +38,59 @@ void addBooksDialog::on_buttonBrowseFolders_clicked()
 
 }
 
-void insertBooks(fs::directory_entry entry, QString tags, QString genre, QString author)
+void addBooksDialog::insertBooks(QString entry, QString tags, QString genre, QString author)
 {
-    QString name = QString::fromStdString(entry.path().stem().string());
-    QString path = QString::fromStdString(entry.path().string());
-    QString folder = QString::fromStdString(entry.path().parent_path().filename().string());
-    QString ext = QString::fromStdString(entry.path().extension().string());
-    long long size = entry.file_size();
+
+    QFileInfo file(entry);
+
+    QString name = file.baseName();
+    QString path = file.absoluteFilePath();
+    QString folder = file.dir().dirName();
+    QString ext = "." + file.suffix();
+    long long size = file.size();
     int pages = common::getPageCount(path);
     queries::insertBooksQuery(name, path, folder, ext, size, pages, tags, genre, author);
 }
 
-template <typename directory_iterator>
-void iterateInsertEntries(directory_iterator dirIterator)
+void addBooksDialog::iterateInsertEntries(QString dir, bool recursive)
 {
-    std::vector<fs::directory_entry> entriesVector;
-    for (fs::directory_entry entry : dirIterator)
+    QList<QString> entriesList;
+    QDirIterator iterator(dir, {"*.pdf", "*.cbr", "*.epub", "*.mobi"}, QDir::Files,
+                          recursive ? QDirIterator::Subdirectories : QDirIterator::NoIteratorFlags);
+
+    while (iterator.hasNext())
     {
-        if(entry.is_regular_file())
-        {
-            if(entry.path().extension() == ".pdf" || entry.path().extension() == ".epub" || entry.path().extension() == ".mobi" || entry.path().extension() == ".cbr")
-            {
-                entriesVector.push_back(entry);
-            }
-        }
+        entriesList.push_back(iterator.next());
     }
-    size_t count = entriesVector.size();
+
+    long count = entriesList.size();
     long counter = 0;
+
     bulkDetailsDialog dialog;
     common::openDialog(&dialog, ":/style.qss");
-// cut this up into more functions
 
     QString tags = dialog.tags.isEmpty() ? "N/A" : dialog.tags;
     QString genre = dialog.genre.isEmpty() ? "N/A" : dialog.genre;
     QString author = dialog.author.isEmpty() ? "N/A" : dialog.author;
 
-
     queries::query.exec("BEGIN TRANSACTION");
-    for(fs::directory_entry &entry : entriesVector)
+    for(QString &entry : entriesList)
     {
         int progress = ((double)counter / count) * 100;
-        progressBar->setValue(progress);
+        ui->progressBar->setValue(progress);
         insertBooks(entry, tags, genre, author);
         counter++;
     }
     queries::query.exec("COMMIT");
-    progressBar->setValue(100);
+    ui->progressBar->setValue(100);
 }
-
 
 void addBooksDialog::on_buttonAdd_clicked()
 {
-    std::string dir = ui->textFolderPath->text().toStdString();
-
-    if(fs::exists(fs::path(dir)))
+    QString dir = ui->textFolderPath->text();
+    if(QFileInfo::exists(dir))
     {
-        if(ui->checkBoxRecursive->isChecked())
-               iterateInsertEntries(fs::recursive_directory_iterator(dir));
-        else
-               iterateInsertEntries(fs::directory_iterator(dir));
+        iterateInsertEntries(dir, ui->checkBoxRecursive->isChecked());
     }
 
 }
-
